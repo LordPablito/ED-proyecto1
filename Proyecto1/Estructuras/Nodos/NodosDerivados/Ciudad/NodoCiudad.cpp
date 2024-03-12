19898cda1a1d80a339a6c2d2b834fb77da5b771f
Pablo Ramirez <104794231+LordPablito@users.noreply.github.com>
2024-03-12 10:57:17 -0600

    Add files via upload
    agregado Nodo de Ciudad

--- Proyecto1/Estructuras/Nodos/NodosDerivados/Ciudad/NodoCiudad.cpp
+++ Proyecto1/Estructuras/Nodos/NodosDerivados/Ciudad/NodoCiudad.cpp
@@ -1,1 +1,29 @@
-﻿#include "NodoCiudad.h"
+#include "NodoCiudad.h"
+
+#include <iostream>
+#include <sstream>
+
+NodoCiudad::NodoCiudad(std::string& Linea)
+{
+    std::string Atributo;
+    std::istringstream StreamLinea(Linea);
+    int Indice = 0;
+    while (std::getline(StreamLinea, Atributo, ';'))
+    {
+        switch (Indice)
+        {
+        case 0:
+            Codigo = std::stoi(Atributo);
+            break;
+        case 1:
+            Nombre = Atributo;
+            break;
+        default: ;
+        }
+        Indice++;
+    }
+}
+
+void NodoCiudad::Mostrar()
+{
+    std::cout << Codigo << Nombre << std::endl;
--- Proyecto1/Estructuras/Nodos/NodosDerivados/Ciudad/NodoCiudad.h
+++ Proyecto1/Estructuras/Nodos/NodosDerivados/Ciudad/NodoCiudad.h
@@ -1,8 +1,21 @@
-﻿#pragma once
+#pragma once
+#include <string>
+
 #include "../../NodoBase.h"
 
-class NodoCiudad: public NodoBase
+class NodoCiudad : public NodoBase
 {
 public:
-    
+    int CodCiudad;
+    std::string Nombre;
+
+    NodoCiudad(const std::string& Linea);
+
+    NodoCiudad(int codCiudad, const std::string& nombre)
+        : CodCiudad(codCiudad),
+          Nombre(nombre)
+    {
+    }
+
+    void Mostrar() override;
 };
