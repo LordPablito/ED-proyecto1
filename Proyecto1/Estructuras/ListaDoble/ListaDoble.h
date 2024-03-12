7e591cd45c3e73df413b911be86d7a2eacf83846
Jose Mariano Soto Núñez <Jmarianosoto@gmail.com>
2024-03-11 19:30:22 -0600

    Implementar lista doble

---
+++ Proyecto1/Estructuras/ListaDoble/ListaDoble.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "../Nodos/NodoBase.h"
+
+class ListaDoble
+{
+public:
+    ListaDoble() { primero = nullptr; }
+    ~ListaDoble();
+
+    void InsertarInicio(NodoBase* NuevoNodo);
+    void InsertarFinal(NodoBase* NuevoNodo);
+    void InsertarPos(NodoBase* NuevoNodo, int pos);
+    bool ListaVacia() { return primero == nullptr; }
+    void BorrarFinal();
+    void BorrarInicio();
+    void BorrarPosicion(int pos);
+    void Mostrar();
+    int largoLista();
+
+private:
+    NodoBase* primero;
+};
+
--- Proyecto1/Proyecto1.cpp
+++ Proyecto1/Proyecto1.cpp
@@ -2,17 +2,22 @@
 #include <iostream>
 #include <string>
 #include "Menu.cpp"
+#include "Estructuras/ListaDoble/ListaDoble.h"
 #include "Estructuras/ListaSimple/ListaSimple.h"
 #include "Estructuras/Nodos/NodosDerivados/Pasillo/NodoPasillo.h"
+#include "Estructuras/Nodos/NodosDerivados/Producto/NodoProducto.h"
 
-static const string DIRECTORIO = "C:/Users/Usuario/Desktop/Proyecto1-20240310T043157Z-001/Proyecto1/Archivos/";
+static const string DIRECTORIO = "../Archivos/";
 
 ListaSimple* CargarPasillos(string NombreArchivo)
 {
     ifstream Archivo;
     ListaSimple* ListaArchivo = new ListaSimple();
     const string Directorio = DIRECTORIO+NombreArchivo;
+    cout<<Directorio<<endl;
     Archivo.open(Directorio);
+    string Basura;
+    getline(Archivo, Basura);
     for(string Linea; getline(Archivo, Linea);)
     {
         ListaArchivo->InsertarFinal(new NodoPasillo(Linea));
@@ -20,11 +25,48 @@ ListaSimple* CargarPasillos(string NombreArchivo)
     return ListaArchivo;
 }
 
+ListaDoble* CargarProductosPasillo(string NombreArchivo)
+{
+    ifstream Archivo;
+    ListaDoble* ListaArchivo = new ListaDoble();
+    const string Directorio = DIRECTORIO+NombreArchivo;
+    cout<<Directorio<<endl;
+    Archivo.open(Directorio);
+    string Basura;
+    getline(Archivo, Basura);
+    for(string Linea; getline(Archivo, Linea);)
+    {
+        ListaArchivo->InsertarFinal(new NodoProducto(Linea));
+    }
+    return ListaArchivo;
+}
+
+ListaDoble* CargarInventario(string NombreArchivo)
+{
+    ifstream Archivo;
+    ListaDoble* ListaArchivo = new ListaDoble();
+    const string Directorio = DIRECTORIO+NombreArchivo;
+    cout<<Directorio<<endl;
+    Archivo.open(Directorio);
+    string Basura;
+    getline(Archivo, Basura);
+    for(string Linea; getline(Archivo, Linea);)
+    {
+        ListaArchivo->InsertarFinal(new NodoProducto(Linea));
+    }
+    return ListaArchivo;
+}
+
 int main()
 {
-    Menu();
-    ListaSimple* Lista = CargarPasillos("Pasillos.txt");
-    Lista->Mostrar();
+    //Menu();
+    ListaSimple* ListaPasillos = CargarPasillos("Pasillos.txt");
+    ListaDoble* ListaProds = CargarProductosPasillo("ProductosPasillos.txt");
+    
+    
+    ListaPasillos->Mostrar();
+    cout<<endl;
+    ListaProds->Mostrar();
     std::cout<<std::endl;
     return 0;
 }
