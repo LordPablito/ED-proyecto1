91ebffdb458b4fc7a262a56f5384756771c9d30d
lordpablito <pabloerl1996@gmail.com>
2024-04-03 01:55:04 -0600

    Guardando cambios locales antes de aplicar cambios remotos

--- Proyecto1/Menu/MenuFunciones.h
+++ Proyecto1/Menu/MenuFunciones.h
@@ -5,6 +5,7 @@
 #include "../Estructuras/ListaSimple/ListaSimple.h"
 #include "../Estructuras/TablaHash/TablaHash.h"
 
+
 class MenuFunciones
 {
 public:
@@ -54,6 +55,7 @@ public:
     static void EliminarCiudad(ListaCircular* ListaCiudades);
     static void EncontrarCiudad(ListaCircular* ListaCiudades);
     static void ModificarCiudad(ListaCircular* ListaCiudades);
+    static void ReporteCiudades(ListaCircular* ListaCiudad);
 #pragma endregion
 
 #pragma region Clientes
@@ -61,5 +63,6 @@ public:
     static void EliminarClientes(TablaHash* TablaClientes, ListaCircular* ListaCiudades);
     static void EncontrarClientes(TablaHash* TablaClientes, ListaCircular* ListaCiudades);
     static void ModificarClientes(TablaHash* TablaClientes, ListaCircular* ListaCiudades);
+    static void GenerarReporteClientes(TablaHash* TablaClientes);
 #pragma endregion 
 };
